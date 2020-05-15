601319c3c5146f1d93925a5660c0c7ba8106c6b5
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-05-15 15:30:16 -0700

    Added PHPMailer
    I added PHPMailer.

--- contact/index.php
+++ contact/index.php
@@ -1,56 +1,34 @@
 <?php
-/**
- * This example shows how to handle a simple contact form.
- */
 
 $msg = '';
-//Don't run this unless we're handling a form submission
 if ((isset($_POST['myName'])) && (empty ($_POST['honeypot']))) {
-/* Since the form has been submitted, let's capture the submission values so we can display them to the user on the success page */
-    $myName = $_POST['myName'];
-    $myEmail = $_POST['myEmail'];
-    $myQuestion = $_POST['myQuestion'];
+    $myName = $_POST['name'];
+    $myEmail = $_POST['email'];
+    $myQuestion = $_POST['question'];
+    $person = $_POST["person"];
     date_default_timezone_set('Etc/UTC');
 
     require '../PHPMailer/PHPMailerAutoload.php';
 
-    //Create a new PHPMailer instance
     $mail = new PHPMailer;
-    //Tell PHPMailer to use SMTP - requires a local mail server
-    //Faster and safer than using mail()
     $mail->isSMTP();
     $mail->Host = 'mail.josevaleriostudent.webhostingforstudents.com';
     $mail->Port = 587;
-    //Set this to true if SMTP host requires authentication to send email
     $mail->SMTPAuth = true;
-    //provide username and password
     $mail->Username = "phpmailer@josevaleriostudent.webhostingforstudents.com";
     $mail->Password = "myPassworD7";
-
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
     $mail->setFrom('phpmailer@josevaleriostudent.webhostingforstudents.com', 'Jose Valerio');
-    //Send the message to yourself, or whoever should receive contact for submissions
     $mail->addAddress('jose.valerio@pcc.edu', 'Jose Valerio');
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
     $mail->addReplyTo($myEmail, $myName);
-        $mail->Subject = 'CAS225 pht contact form';
-        //Keep it simple - don't use HTML
+        $mail->Subject = 'Ace in the Hole contact form';
         $mail->isHTML(true);
-        //Build a simple message body
         $mail->Body = <<<EOT
 Email: $myEmail<br>
 Name: $myName<br>
 Question: $myQuestion
 EOT;
 
-        //Send the message, check for errors
         if (!$mail->send()) {
-            //The reason for failing to send will be in $mail->ErrorInfo
-            //but you shouldn't display errors to users - process the error, log it on your server.
             echo "Mailer error" . $mail->ErrorInfo;
         } else {
           include 'success.html.php';
