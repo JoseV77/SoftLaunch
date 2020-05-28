9c850e59d7f45e5aede34723ea52ffb52b841067
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-05-28 14:26:55 -0700

    alt text
    I added all of the alt text for the images

--- includes/slideshow.html.php
+++ includes/slideshow.html.php
@@ -4,78 +4,92 @@
 
     <div class="mySlides fade" id="front">
         <div class="number">1 / <?php echo $pics; ?></div>
-        <img src="images/ace1.jpg" alt="placeholder">
+        <img src="images/ace1.jpg" alt="swimming_triathlon">
     </div>
 
     <div class="mySlides fade">
         <div class="number">2 / <?php echo $pics; ?></div>
-        <img src="images/ace2.jpg" alt="placeholder">
+        <img src="images/ace2.jpg" alt="marathon">
     </div>
 
     <div class="mySlides fade">
         <div class="number">3 / <?php echo $pics; ?></div>
-        <img src="images/ace3.jpg" alt="placeholder">
+        <img src="images/ace3.jpg" alt="long_course_bike">
     </div>
 
     <div class="mySlides fade">
         <div class="number">4 / <?php echo $pics; ?></div>
-        <img src="images/ace4.jpg" alt="placeholder">
+        <img src="images/ace4.jpg" alt="beach_triathlon">
     </div>
+
     <div class="mySlides fade">
         <div class="number">5 / <?php echo $pics; ?></div>
-        <img src="images/ace5.jpg" alt="placeholder">
+        <img src="images/ace5.jpg" alt="bike_triathlon">
     </div>
+
     <div class="mySlides fade">
         <div class="number">6 / <?php echo $pics; ?></div>
-        <img src="images/ace6.jpg" alt="placeholder">
+        <img src="images/ace6.jpg" alt="beach_event">
     </div>
+
     <div class="mySlides fade">
         <div class="number">7 / <?php echo $pics; ?></div>
-        <img src="images/ace7.jpg" alt="placeholder">
+        <img src="images/ace7.jpg" alt="traithlon_marathon">
     </div>
+
     <div class="mySlides fade">
         <div class="number">8 / <?php echo $pics; ?></div>
-        <img src="images/ace8.jpg" alt="placeholder">
+        <img src="images/ace8.jpg" alt="running_triathlon">
     </div>
+
     <div class="mySlides fade">
         <div class="number">9 / <?php echo $pics; ?></div>
-        <img src="images/ace9.jpg" alt="placeholder">
+        <img src="images/ace9.jpg" alt="bike_race">
     </div>
+
     <div class="mySlides fade">
         <div class="number">10 / <?php echo $pics; ?></div>
         <img src="images/ace10.jpg" alt="placeholder">
     </div>
+
     <div class="mySlides fade">
         <div class="number">11 / <?php echo $pics; ?></div>
-        <img src="images/ace11.jpg" alt="placeholder">
+        <img src="images/ace11.jpg" alt="running_race">
     </div>
+
     <div class="mySlides fade">
         <div class="number">12 / <?php echo $pics; ?></div>
         <img src="images/ace12.jpg" alt="placeholder">
     </div>
+
     <div class="mySlides fade">
         <div class="number">13 / <?php echo $pics; ?></div>
-        <img src="images/ace13.jpg" alt="placeholder">
+        <img src="images/ace13.jpg" alt="triathlon">
     </div>
+
     <div class="mySlides fade">
         <div class="number">14 / <?php echo $pics; ?></div>
-        <img src="images/ace14.jpg" alt="placeholder">
+        <img src="images/ace14.jpg" alt="bikes">
     </div>
+
     <div class="mySlides fade">
         <div class="number">15 / <?php echo $pics; ?></div>
-        <img src="images/ace15.jpg" alt="placeholder">
+        <img src="images/ace15.jpg" alt="running">
     </div>
+
     <div class="mySlides fade">
         <div class="number">16 / <?php echo $pics; ?></div>
-        <img src="images/ace16.jpg" alt="placeholder">
+        <img src="images/ace16.jpg" alt="olympic">
     </div>
+
     <div class="mySlides fade">
         <div class="number">17 / <?php echo $pics; ?></div>
-        <img src="images/ace17.jpg" alt="placeholder">
+        <img src="images/ace17.jpg" alt="10k">
     </div>
+
     <div class="mySlides fade">
         <div class="number">18 / <?php echo $pics; ?></div>
-        <img src="images/ace18.jpg" alt="placeholder">
+        <img src="images/ace18.jpg" alt="sprint_triathlon">
     </div>
 
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
