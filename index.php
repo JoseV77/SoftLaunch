213b8c9e96300f00acabfb40c79f83eca149a6fb
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-06-01 19:29:38 -0700

    Feeds Adjustments
    I adjusted the feeds to fit and align with each other

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                     </div>
 
                     <div id="twitter-feed">
-                    <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+                    <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a> <script async src="https://platform.twitter.com/widgets.js"></script> 
                     </div>
 
 
