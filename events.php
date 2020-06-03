01833a235a70575be18376f6569e5aa693d6bde2
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-06-03 16:51:14 -0700

    Events underlines
    I added underlines to the titles of each information

--- events.php
+++ events.php
@@ -86,69 +86,71 @@
 
             <h2 id="details">Course Details</h2>
             <ul id="detailsList">
-                <li><strong>Long Course SWIM - 1.2mi:</strong> Participants will make two counter-clockwise loops. Large
+                <li><strong><span class="titles">Long Course SWIM - 1.2mi:</span></strong> Participants will make two counter-clockwise loops. Large
                     buoys will mark
                     the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support
                     will be present on the beach.</li>
-                <li><strong>Long Course BIKE - 58 Miles:</strong> A scenic point-to-point course that travels over
+                <li><strong><span class="titles">Long Course BIKE - 58 Miles:</span></strong> A scenic point-to-point course that travels over
                     gently rolling hills
                     prior to three hard climbs. The bike course will be marked with large directional signage and there
                     will be course marshals at key intersections to help direct you.</li>
-                <li><strong>Long Course RUN - 13.1mi:</strong> A mostly flat loop course on widely paved bike paths that
+                <li><strong><span class="titles">Long Course RUN - 13.1mi:</span></strong> A mostly flat loop course on widely paved bike paths that
                     traverse
                     through and around this beautiful and scenic destination resort (two hills total with a minimal
                     elevation gain).</li>
-                <li><strong>OLYMPIC SWIM - 1,500 meters -</strong> Participants will make two counter-clockwise loops.
+                <li><strong><span class="titles">OLYMPIC SWIM - 1,500 meters:</span></strong> Participants will make two counter-clockwise loops.
                     Large buoys will
                     mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical
                     support will be present on the beach.</li>
-                <li><strong>OLYMPIC BIKE - 28mi -</strong> A scenic point-to-point course that travels over gently
+                <li><strong><span class="titles">OLYMPIC BIKE - 28mi:</span></strong> A scenic point-to-point course that travels over gently
                     rolling hills. The
                     bike course will be marked with large directional signage and course marshals will be present at key
                     intersections.</li>
-                <li><strong>OLYMPIC RUN - 10K -</strong> A mostly flat loop course on widely paved bike paths that
+                <li><strong><span class="titles">OLYMPIC RUN - 10K:</span></strong> A mostly flat loop course on widely paved bike paths that
                     traverse through and
                     around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).
                 </li>
-                <li><strong>Sprint:</strong> Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike
+                <li><strong><span class="titles">Sprint:</span></strong> Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike
                     Course as the Olympic
                     distance and a 5km run.</li>
-                <li><strong>Try-A-Tri:</strong> This novice race is designed for the first time triathlete, those new to
+                <li><strong><span class="titles">Try-A-Tri:</span></strong> This novice race is designed for the first time triathlete, those new to
                     the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance,
                     (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2
                     loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).
                 </li>
-                <li><strong>Half Marathon COURSE:</strong> Half-Marathon (13.1-miles): Half-Marathon event starts and
+                <li><strong><span class="titles">Half Marathon COURSE:</span></strong> Half-Marathon (13.1-miles): Half-Marathon event starts and
                     finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can
                     enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment.
                     Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin
                     once the results have been certified</li>
-                <li><strong>10k COURSE:</strong> The 10K event starts and finishes in the Athletes Village. The paths
+                <li><strong><span class="titles">10k COURSE:</span></strong> The 10K event starts and finishes in the Athletes Village. The paths
                     are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has
                     only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to
                     the roaring cheers of the crowd.</li>
-                <li><strong>Splash n' Dash:</strong> Kids event for participants ages 4 to 15. This is a 100 yard swim
+                <li><strong><span class="titles">Splash n' Dash:</span></strong> Kids event for participants ages 4 to 15. This is a 100 yard swim
                     and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</li>
             </ul>
 
             <h2 id="bring">What to Bring</h2>
-            <p>Watch the weather closely. The show goes on no matter what the weather is doing. <br><br>You must wear
-                your chip timing piece during the entire event. You will be given a band that will hold your chip timing
+            <p>Watch the weather closely. The show goes on no matter what the weather is doing. 
+                <br><br>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing
                 piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have
                 your Chip Timing piece on before you start the race and be sure to step over the timing mats after each
                 segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit
-                otherwise, you will have a very difficult time getting off your wet suit. <br><br><strong>Swim:</strong>
-                Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no
+                otherwise, you will have a very difficult time getting off your wet suit. 
+                <br><br><strong><span class="titles">Swim:</span></strong> Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no
                 wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring
-                your own goggles. <br><br><strong>Bike:</strong> A biking helmet is mandatory. You will also receive 2
+                your own goggles. <br><br>
+                <strong><span class="titles">Bike:</span></strong> A biking helmet is mandatory. You will also receive 2
                 stickers in your package with your race number on them. The small sticker should go on the front of your
                 helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For
-                safety reasons, be sure to have end-caps on the end of your handle-bars. <br><br><strong>Run:</strong>
-                You must finish the race with your bib number on the front of you. Some people choose to pin it on at
+                safety reasons, be sure to have end-caps on the end of your handle-bars. 
+                <br><br><strong><span class="titles">Run:</span></strong> You must finish the race with your bib number on the front of you. Some people choose to pin it on at
                 the beginning of the race and have it on for the whole event so they don't have to worry about it.
                 Others pin it on a singlet that they put on once they finish the swim before they head out for the bike.
                 Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before
-                they leave for the run. Use whatever option feels best for you. <br><br>Remember to bring a change of
+                they leave for the run. Use whatever option feels best for you. 
+                <br><br>Remember to bring a change of
                 clothing so you can enjoy the post-event festivities.</p>
 
             <h2 id="packet">Packet Pick Up</h2>
@@ -203,6 +205,8 @@
                 as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
 
         </div>
+
+        <button onclick="goUp()" id="top" title="Go to top">Top</button>
 
     </main>
 
